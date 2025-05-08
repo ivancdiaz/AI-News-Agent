66ea0d9fc71c871c204b24860c93cc3453cebc4a
Ivan Diaz <idiaz623@gmail.com>
2025-05-08 15:31:38 -0700

    feat: add Article model and NewsService with sample articles

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AI.News.Agent.Services;
+using AI.News.Agent.Output;
+
+
+namespace AI.News.Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newsService = new NewsService();
+            var outputService = new OutputService();
+
+            var articles = newsService.GetSampleArticles();
+            outputService.DisplayArticles(articles);
+        }
+    }
+}
