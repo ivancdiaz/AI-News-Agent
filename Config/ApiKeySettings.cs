c1440aa5c1c2b045cc89bc4733017ed5890ab945
Ivan Diaz <idiaz623@gmail.com>
2025-06-23 14:31:39 -0700

    refactor: move API keys and settings to config classes

---
+++ Config/ApiKeySettings.cs
@@ -0,0 +1,8 @@
+namespace AI.News.Agent.Config
+{
+    public class ApiKeySettings
+    {
+        public string NewsApiKey { get; set; }
+        public string HuggingFaceApiKey { get; set; }
+    }
+}
